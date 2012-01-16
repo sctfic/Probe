25f7a69cc0ec0e8f38f2b8478410630902c104d7
alban lopez <alban.lopez+github@gmail.com>
2012-01-16 15:30:03 +0100

    Admin interface Step 0

--- StationScript/index.php
+++ StationScript/index.php
@@ -2,12 +2,6 @@
 // StationScript
 $workingFolder = dirname(__FILE__).DIRECTORY_SEPARATOR;
 $stationConfig = eval('return '.file_get_contents($workingFolder.'/../stations.conf').';');
-// file_put_contents ($stationFolder.'Station.conf',
-// 	var_export(
-// 		array(
-// 			'VP2-Inside'=>array('IP'=>'VP2','Port'=>22222,'type'=>'VP2-IP','Last_DMPAFT'=>'0'),
-// 			'VP2-Gtd'=>array('IP'=>'nas-alban.no-ip.org','Port'=>22222,'type'=>'VP2-IP','Last_DMPAFT'=>'0'),
-// 		), true));
 
 foreach($stationConfig as $configKey=>$configValue)
 {
@@ -16,7 +10,6 @@ foreach($stationConfig as $configKey=>$configValue)
 
 	if ($configValue['type']=='VP2-IP')
 	{
-// 		echo "\n++++++++++++++++++++++\t{$configKey}\t++++++++++++++++++++++++++++++++++++++++++++\n";
 		echo sprintf ("\n%'+40s %10s %'+40s\n", "", $configKey, "");
 
 		$station = new station($stationConfig, $configKey);
@@ -24,13 +17,14 @@ foreach($stationConfig as $configKey=>$configValue)
 		{
 			$station->Waiting( 0, _( sprintf('[Succès] Ouverture de la connexion à %s', $configKey) ) );
 
-			var_export ($station->Read_Configs());
+///			var_export ($station->Read_Configs());
 /// 			$station->Get_HILOWS_Raw();	// OK
 /// 			$station->Get_LOOP_Raw();	// OK
 /// 			$station->Get_DMPAFT_Raw();	// OK
-/// 			if (abs(strtotime($station->fetchStationTime()) - strtotime(date('Y/m/d H:i:s'))) > 3)
-/// 			  if ($station->updateStationTime())
-/// 					$Station->Waiting (0,'Clock synch.');
+
+// 			if (abs(strtotime($station->fetchStationTime()) - strtotime(date('Y/m/d H:i:s'))) > 3)	// OK
+// 			  if ($station->updateStationTime())									// OK
+// 					$station->Waiting (0,'Clock synch.');							// OK
 
 			if ($station->closeConnection())
 				$station->Waiting( 0, sprintf( _('[Succès] Fermeture de %s correcte.'), $configKey ) );
@@ -38,8 +32,7 @@ foreach($stationConfig as $configKey=>$configValue)
 				$station->Waiting( 0, sprintf( _('[Échec] Fermeture de %s.'), $configKey ) );
 		}
 			else
-				$station->Waiting( 0, sprintf( _('[Échec] Impossible de se connecter à %s.'), $configKey ) );
+				$station->Waiting( 0, sprintf( _('[Échec] Impossible de se connecter à %s par %s:%s.'), $configKey, $configValue['IP'], $configValue['Port']) );
 	}
 }
-
 ?>
--- WebAdmin/index.php
+++ WebAdmin/index.php
@@ -1,3 +1,101 @@
 <?php
-
+// WebAdmin
+$workingFolder = dirname(__FILE__).DIRECTORY_SEPARATOR;
+if (is_file($workingFolder.'/../stations.conf'))
+	$stationConfig = eval('return '.file_get_contents($workingFolder.'/../stations.conf').';');
+else
+	file_put_contents ($workingFolder.'/../stations.conf',
+		var_export( array ( 'VP2-Inside' =>
+			array (
+				'IP' => 'VP2',
+				'Port' => 22222,
+				'type' => 'VP2-IP',
+				'Last_DMPAFT' => '',
+				'Last_Connected' => '',
+				'Last_Read_Config' => '',
+				'Last_SETTIME' => '',
+				'AVERAGE_TEMP' => '',
+				'VER' => '',
+				'NVER' => '',
+				'RXCHECK' =>   array ( ),
+				'BARDATA' =>   array ( ),
+				'BAR_OFFSET' => 0,
+				'BAR_CAL' => 0,
+				'LATITUDE' => 0,
+				'LONGITUDE' => 0,
+				'ELEVATION' => 0,
+				'TIME_ZONE' => 0,
+				'GTM_OFFSET' => '',
+				'GTM_OR_ZONE' => '',
+				'UNIT_BITS' =>  array ( ),
+				'SETUP_BITS' => array ( ),
+				'RAIN_SEASON_START' => 0,
+				'ARCHIVE_PERIOD' => 0,
+				'TEMP_IN_CAL' => 0,
+				'WIND_DIR_CAL' => 0,
+				'TEMP_OUT_CAL' => 0,
+			),), true));
+if (is_file($workingFolder.'/../WsWds.conf'))
+	$WsWdsConfig = eval('return '.file_get_contents($workingFolder.'/../WsWds.conf').';');
+else
+	file_put_contents ($workingFolder.'/../stations.conf',
+		var_export( array (
+			'AdminInterface' =>
+			array (
+				'Username' => 'WsWds',
+				'Password' => '',
+			),
+			'DataBase' =>
+			array (
+				'useIt' => true,
+				'Serveur' => '127.0.0.1',
+				'Port' => 999,
+				'Username' => 'WsWds',
+				'Password' => '',
+			),
+			'LocalFile' =>
+			array (
+				'useIt' => True,
+				'FilesNames' => 'VP2-%n%.data',
+				'Port' => 21,
+				'Username' => 'WsWds',
+				'Password' => '',
+			),
+			'FTP' =>
+			array (
+				'useIt' => False,
+				'Serveur' => '127.0.0.1',
+				'Port' => 21,
+				'Username' => 'WsWds',
+				'Password' => '',
+			),
+			'SSH' =>
+			array (
+				'useIt' => False,
+				'Serveur' => '127.0.0.1',
+				'Port' => 22,
+				'Username' => 'WsWds',
+				'Password' => '',
+			),
+			'WebInterface' =>
+			array (
+				'useIt' => True,
+				'Serveur' => '127.0.0.1',
+				'Port' => 80,
+				'Username' => 'WsWds',
+				'Password' => '',
+			),
+			'' =>
+			array (
+				'useIt' => False,
+				'Serveur' => '127.0.0.1',
+				'Port' => 21,
+			),
+			'NOAA' =>
+			array (
+				'useIt' => True,
+				'Serveur' => '127.0.0.1',
+				'Port' => 80,
+			),
+			), true));
 ?>
