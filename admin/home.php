4e67549d4990a8ce124f7efc00d8f38d67627b7b
Édouard Lopez <dev+github@edouard-lopez.com>
2012-03-14 01:43:45 +0100

    mock-up: layout with <dl>/<details>/<summary>, edit link

--- admin/home.php
+++ admin/home.php
@@ -1,35 +1,41 @@
-<article>
+<article id="admin-station">
 <?php
-
     require_once($GLOBALS['workingFolder'].'../resources/php/toolbox.phpc');
     require_once($GLOBALS['workingFolder'].'../resources/php/configManager.phpc');
     $stationConf = configManager::getConfig('station');
 
     foreach ($stationConf as $stationId => $stationInfos) {?>
         <section id="<?php echo $stationId;?>">
-            <h3><?php echo $stationId;?></h3>
-            <form action="edit.php" method="get">
-                <fieldset>
-                <input type="hidden" name="config" value="station" />
-                <input type="hidden" name="stationId" value="<?php echo $stationId;?>" />
-                <?php
-                    foreach ($stationInfos as $infoKey => $infoVal) {
-                    ?>
-                        <label for="<?php echo $infoKey;?>"><?php echo $infoKey;?></label>
-                        <input id="<?php echo $infoKey;?>" name="<?php echo $infoKey;?>" type="text" readonly />
-                    <?php
-                    }
-                ?>
-                </fieldset>
-            </form>
+            <h3><?php echo $stationId;?>
+                    <a href='./edit/station:<?php echo $stationId;?>'><?php echo sprintf(_('[edit <i class="hidden">%s</i> properties]'), $stationId);?></a>
+            </h3>
+            <summary><b><?php echo sprintf('%s: ', _('Model'));?></b><i><?php echo $stationInfos['type'];?></i></summary>
+            <details>
+                <form action="edit.php" method="get">
+                    <fieldset>
+                        <input type="hidden" name="config" value="station" />
+                        <input type="hidden" name="stationId" value="<?php echo $stationId;?>" />
+                        <dl>
+                        <?php
+                            foreach ($stationInfos as $infoKey => $infoVal) {
+                            ?>
+                                <dt><label for="<?php echo strtolower($infoKey);?>"><?php echo _($infoKey);?></label></dt>
+                                <dd><input disabled
+                                    id="<?php echo $infoKey;?>"
+                                    name="<?php echo $infoKey;?>"
+                                    type="text"
+                                    value="<?php echo $infoVal;?>"
+                                /></dd>
+                            <?php
+                            }
+                        ?>
+                        </dl>
+                    </fieldset>
+                </form>
+            </details>
         </section>
     <?php
     }
 ?>
-    <section>
-        <h3><?php echo _('Station One');?></h3>
-    </section>
 </article>
-<progress value="29" max="400"></progress><br />
 <a href="./?stop" class='btn'><?php echo _('logOut'); ?></a>
-<meter value="7" min="0" max="19"></meter><br />
