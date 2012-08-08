30a68d4e5fa6fa854466eed83224038af4e295e4
Sctfic (Alban LOPEZ) <alban.lopez+github@gmail.com>
2012-08-09 00:09:39 +0200

    Update query for EAV config saving

--- application/models/dbconfig.php
+++ application/models/dbconfig.php
@@ -86,8 +86,11 @@ class dbconfig extends CI_Model {
 // 		$query = 'INSERT INTO `TR_CONFIG` (CFG_STATION_ID, CFG_LABEL, CFG_VALUE, CFG_LAST_WRITE) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE (CFG_VALUE, CFG_LAST_WRITE) VALUES (?, ?);';
 // 		$prep = $this->db->conn_id->prepare($query);
 		foreach ($conf as $label => $value) {
-			$query = 'INSERT INTO `TR_CONFIG` (CFG_STATION_ID, CFG_LABEL, CFG_VALUE, CFG_LAST_WRITE) VALUES ('.$id.', \''.$label.'\', \''.$this->CI->db->quote($value).'\', \''.date ("Y-m-d H:i:s").'\') 
-			ON DUPLICATE KEY UPDATE CFG_VALUE = \''.$this->CI->db->quote($value).'\', CFG_LAST_WRITE = \''.date ("Y-m-d H:i:s").'\';';
+// 			$query = 'INSERT INTO `TR_CONFIG` (CFG_STATION_ID, CFG_LABEL, CFG_VALUE, CFG_LAST_WRITE) VALUES ('.$id.', \''.$label.'\', \''.$this->CI->db->quote($value).'\', \''.date ("Y-m-d H:i:s").'\') 
+// 			ON DUPLICATE KEY UPDATE CFG_VALUE = \''.$this->CI->db->quote($value).'\', CFG_LAST_WRITE = \''.date ("Y-m-d H:i:s").'\';';
+// http://codeigniter.com/user_guide/database/queries.html
+			$query = 'INSERT INTO `TR_CONFIG` (CFG_STATION_ID, CFG_LABEL, CFG_VALUE, CFG_LAST_WRITE) VALUES ('.$id.', \''.$label.'\', '.$this->db->escape($value).', \''.date ("Y-m-d H:i:s").'\') 
+			ON DUPLICATE KEY UPDATE CFG_LAST_WRITE = IF('.$this->db->escape($value).' != CFG_VALUE, \''.date ("Y-m-d H:i:s").'\',CFG_LAST_WRITE),  CFG_VALUE = '.$this->db->escape($value).';';
 			$this->db->query($query);
 		
 			//Associer des valeurs aux place holders
--- application/models/dbdata.php
+++ application/models/dbdata.php
@@ -14,7 +14,9 @@ class dbdata extends CI_Model {
 		$DB = $this->load->database($base, TRUE);
 		$this->prep_EAV = $this->db->conn_id->prepare('REPLACE INTO :table (ID, VALUE, SEN_ID) VALUES (:utc, :val, :sensorID);');
 		$this->prep_SENSOR = $this->db->conn_id->prepare('REPLACE INTO `TR_SENSOR` (SEN_ID, , ) VALUES (:sensorID, :name, :descript, :min, :max, :error, :unit);');
-		$this->prep_ROW = $this->db->conn_id->prepare('REPLACE INTO `TA_VARIOUS` (, , ) VALUES ();');
+		$this->prep_ROW = $this->db->conn_id->prepare('REPLACE  INTO `TA_VARIOUS` 
+			(`VAR_ID` ,`VAR_SAMPLE_RAINFALL` ,`VAR_SAMPLE_RAINFALL_HIGHT` ,`VAR_PRESSURE` ,`VAR_SOLAR_RADIATION` ,`VAR_SOLAR_RADIATION_HIGHT` ,`VAR_WIND_SPEED` ,`VAR_WIND_SPEED_HIGHT` ,`VAR_WIND_SPEED_HIGHT_DIR` ,`VAR_WIND_SPEED_DOMINANT_DIR` ,`VAR_UV_INDEX` ,`VAR_UV_INDEX_HIGHT` ,`VAR_FORECAST_RULE` ,`VAR_RAIN`)
+			VALUES (:id, :rainfall, :max_rainfall, :pressure, :srad, :max_srad, :wspeed, :max_wspeed, :dir_higtspeed, :dir_dominant, :uv, :max_uv, :forcast, :rain);');
 	}
 	function parse_Data(){
 	
@@ -27,18 +29,17 @@ class dbdata extends CI_Model {
 		
 	}
 	function insert_SENSOR() {
-		$this->prep_SENSOR->execute(array('sensorID'=>, 'name'=>, 'descript'=>, 'min'=>, 'max'=>, 'error'=>, 'unit'=>));
+		$this->prep_SENSOR->execute(array('sensorID'=>0, 'name'=>'none', 'descript'=>'', 'min'=>0, 'max'=>100, 'error'=>255, 'unit'=>'%'));
 		
 	}
 	function insert_ROW() {
-		
+		$this->prep_SENSOR->execute(array('id'=>0, 'rainfall'=>0, 'max_rainfall'=>0, 'pressure'=>1013, 'srad'=>1, 'max_srad'=>1, 'wspeed'=>1, 'max_wspeed'=>2, 'dir_higtspeed'=>1, 'dir_dominant'=>1, 'uv'=>1, 'max_uv'=>1, 'forcast'=>1, 'rain'=>0 ));
+
 	}
 	function get_Last_Date() {
-		$date = $this->db->query('SELECT MAX( VAR_ID ) FROM  `TA_VARIOUS` LIMIT 10';
+		$date = $this->db->query('SELECT MAX( VAR_ID ) FROM  `TA_VARIOUS` LIMIT 10');
 		if (count(result_array($date))==1)
-			return $date->VAR_ID;
-		print_r($date);
-
-		log_message('db', "");
+			return end($date->VAR_ID);
+		log_message('db', 'trop de resultat : '.print_r($date));
 	}
 }
