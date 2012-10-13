0ec7a65b9424fcf868fb9d8f551a19e9929ba8de
Édouard Lopez <dev+github@edouard-lopez.com>
2012-10-13 20:52:27 +0200

    improve layout by using bootstrap class

--- application/views/setup/dbms.php
+++ application/views/setup/dbms.php
@@ -1,36 +1,77 @@
-<?=form_open('setup/installer/setupDbms', array('class' => 'setup', 'id' => 'dbms'))?>
+<?=form_open('setup/installer/setupDbms', array('class' => 'setup form-horizontal', 'id' => 'dbms'))?>
 	<fieldset>
 	<?=validation_errors()?>
 
 	<!-- Server host/IP -->
-	<?=form_label( sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.dbms.ip'), i18n('required'), i18n('&nbsp;:')), 'dbms-ip')?>
-	<?=form_input( 'dbms-ip', $dbmsIp, sprintf('placeholder="%s"',i18n('setup.dbms.ip.placeholder') ) )?>
+	<div class="control-group">
+		<label class="control-label" for="dbms-ip">
+			<?=sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.dbms.ip'), i18n('required'), i18n('&nbsp;:')) ?>
+		</label>
+		<div class="controls">
+			<input type="range" id="dbms-ip" value="<?=$dbmsIp?>" class="input-medium" placeholder="<?=i18n('setup.dbms.ip.placeholder')?>">
+		</div>
+	</div>
 
 	<!-- Server port (range between: 1-65535) -->
-	<?=form_label( sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.dbms.port'), i18n('required'), i18n('&nbsp;:')), 'dbms-port')?>
-	<?=form_input( 'dbms-port', $dbmsPort, sprintf('placeholder="%s"',i18n('setup.dbms.port.placeholder') ) )?>
+	<div class="control-group">
+		<label class="control-label" for="dbms-port">
+			<?=sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.dbms.port'), i18n('required'), i18n('&nbsp;:')) ?>
+		</label>
+		<div class="controls">
+			<input type="range" id="dbms-port" value="<?=$dbmsPort?>" class="input-mini" placeholder="<?=i18n('setup.dbms.port.placeholder')?>">
+		</div>
+	</div>
 
 	<!-- Engine: MySQL vs. SQLite  -->
-	<?=form_label( sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.dbms.engine-mysql'), i18n('required'), i18n('&nbsp;:')), 'dbms-engine-mysql')?>
-	<?=form_radio( array( 'name' => 'dbms-engine', 'id' => 'dbms-engine-mysql', 'value' => 'mysql', 'checked' => true ) )?>
-	<?=form_label( sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.dbms.engine-sqlite'), i18n('required'), i18n('&nbsp;:')), 'dbms-engine-sqlite')?>
-	<?=form_radio( array( 'name' => 'dbms-engine', 'id' => 'dbms-engine-sqlite', 'value' => 'sqlite', 'checked' => false ) )?>
+	<div class="control-group">
+		<label class="control-label">
+			<?=sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.dbms.engine'), i18n('required'), i18n('&nbsp;:')) ?>
+		</label>
+		<div class="controls">
+			<label class="control-label inline" for="dbms-engine-mysql">
+				<input type="radio" name="dbms-engine" id="dbms-engine-mysql" value="<?=$dbmsPort?>" checked class="input-mini" >
+				<?=sprintf('%s <span class="hidden">(%s)</span>', i18n('setup.dbms.engine-mysql'), i18n('required')) ?>
+			</label>
+			<label class="control-label inline" for="dbms-engine-sqlite">
+				<input type="radio" name="dbms-engine" id="dbms-engine-sqlite" value="<?=$dbmsPort?>" class="input-mini" >
+				<?=sprintf('%s <span class="hidden">(%s)</span>', i18n('setup.dbms.engine-sqlite'), i18n('required')) ?>
+			</label>
+		</div>
+	</div>
 
-	<!-- Server admin user -->
-	<?=form_label( sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.dbms.username'), i18n('required'), i18n('&nbsp;:')), 'dbms-username')?>
-	<?=form_input( 'dbms-username', $dbmsUsername, sprintf('placeholder="%s"',i18n('setup.dbms.password.placeholder') ) )?>
+
+	<div class="control-group">		
+	<!-- Server admin username -->
+		<label class="control-label" for="dbms-username">
+			<?=sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.dbms.username'), i18n('required'), i18n('&nbsp;:'))?>
+		</label>
+		<div class="controls">
+			<input type="text" id="dbms-username" required value="<?=$dbmsUsername?>" class="input-large" placeholder="<?=i18n('setup.dbms.username.placeholder')?>">
+		</div>
+	</div>
+
+	<div class="control-group">		
 	<!-- Server admin password -->
-	<?=form_label( sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.dbms.password'), i18n('required'),i18n('&nbsp;:')), 'dbms-password')?>
-	<?= sprintf('');//form_password( 'password', $username, sprintf('placeholder="%s" %s',i18n('longer is better'), getStatus('password') ) )?>
-	<?=form_password( 'dbms-password', $dbmsPassword, sprintf('placeholder="%s"',i18n('setup.dbms.password.placeholder') ) )?>
+		<label class="control-label" for="dbms-password">
+			<?=sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.dbms.password'), i18n('required'), i18n('&nbsp;:'))?>
+		</label>
+		<div class="controls">
+			<input type="text" id="dbms-password" required value="<?=$dbmsPassword?>" class="input-large" placeholder="<?=i18n('setup.dbms.password.placeholder')?>">
+		</div>
+	</div>
 	
-	<!-- Server admin baseName -->
-	<?=form_label( sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.dbms.basename'), i18n('required'),i18n('&nbsp;:')), 'dbms-basename')?>
-	<?= sprintf('');//form_basename( 'basename', $username, sprintf('placeholder="%s" %s',i18n('longer is better'), getStatus('basename') ) )?>
-	<?=form_password( 'dbms-basename', $dbmsBaseName, sprintf('placeholder="%s"',i18n('setup.dbms.basename.placeholder') ) )?>
+	<div class="control-group">
+	<!-- Server database name -->
+		<label class="control-label" for="dbms-database-name">
+			<?=sprintf('%s <span class="hidden">(%s)</span>', i18n('setup.dbms.database-name'), i18n('&nbsp;:'))?>
+		</label>
+		<div class="controls">
+			<input type="text" id="dbms-database-name" value="<?=$dbmsDatabaseName?>" class="input-large" placeholder="<?=i18n('setup.dbms.database-name.placeholder')?>">
+		</div>
+	</div>
 	
 	
-	<?=form_submit('authentificate', i18n('setup.dbms.do-it'), 'class="btn"')?>
+	<?=form_submit('configure', i18n('setup.dbms.configure'), 'class="btn pull-right"')?>
 	<!--keygen name="security" /-->
 	</fieldset>
 <?=form_close()?>
