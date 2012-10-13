216926069c5e0b453241027c7ef66c06acb2703b
Édouard Lopez <dev+github@edouard-lopez.com>
2012-10-13 11:15:04 +0200

    view for admin user setup

--- application/views/setup/admin-user.php
+++ application/views/setup/admin-user.php
@@ -2,26 +2,21 @@
     <fieldset>
     <?=validation_errors()?>
 
-    <?=form_label( sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.admin.username'), i18n('required'), i18n('&nbsp;:')), 'admin-username')?>
-    <?=form_input( 'admin-username', $adminUsername, sprintf('placeholder="%s"',i18n('setup.admin.username.placeholder') ) )?>
+    <?=form_label( sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.admin-user.username'), i18n('required'), i18n('&nbsp;:')), 'admin-username')?>
+    <?=form_input( array( 'name' => 'admin-username', 'id' => 'admin-username', 'value' => $adminUsername, 'placeholder' => i18n('setup.dbms.username.placeholder') ) )?>
 
-    <?=form_label( sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.admin.password'), i18n('required'),i18n('&nbsp;:')), 'admin-password')?>
+    <?=form_label( sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.admin-user.password'), i18n('required'),i18n('&nbsp;:')), 'admin-password')?>
     <?= sprintf('');//form_password( 'password', $username, sprintf('placeholder="%s" %s',i18n('longer is better'), getStatus('password') ) )?>
-    <?=form_password( 'admin-password', $adminPassword, sprintf('placeholder="%s"',i18n('setup.admin.password.placeholder') ) )?>
+    <?=form_input( array( 'name' => 'admin-password', 'id' => 'admin-password', 'value' => $adminPassword, 'placeholder' => i18n('setup.dbms.password.placeholder') ) )?>
 
     <?php
     if ( _empty($this->config->item('ws:username') ) ) {?>
-        <?=form_label( sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.admin.password-confirmation'), i18n('required'),i18n('&nbsp;:')), 'confirm')?>
+        <?=form_label( sprintf('%s <span class="hidden">(%s)</span>%s', i18n('setup.admin-user.password-confirmation'), i18n('required'),i18n('&nbsp;:')), 'confirm')?>
         <?= sprintf('');//form_password( 'confirm', '', sprintf('placeholder="%s" %s',i18n('so we can prevent mistakes'), getStatus('confirm') ) )?>
-        <?=form_password( 'admin-password-confirm', $adminPasswordConfirmation, sprintf('placeholder="%s"',i18n('setup.admin.password-confirmation.placeholder') ) )?>
+        <?=form_input( array( 'name' => 'admin-password-confirm', 'id' => 'admin-password-confirm', 'value' => $adminPasswordConfirmation, 'placeholder' => i18n('setup.dbms.password-confirm.placeholder') ) )?>
     <?php } ?>
 
-    <?=form_submit('setup-admin-user', i18n('setup.admin.user.do-it'), 'class="btn"')?>
+    <?=form_submit('setup-admin-user', i18n('setup.admin-user.do-it'), 'class="btn"')?>
     <!--keygen name="security" /-->
     </fieldset>
-    <aside class="js time-indicator">
-        <div class="now"><?=date('Y-m-d H:i')?>
-        <!-- <img class="time-indicator" src="/themes/icons/weather-clear.png" alt="<?=i18n("sunny weather")?>?>" /> -->
-        </div>
-    </aside>
 <?=form_close()?>
