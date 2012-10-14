1d7b2b69dea22ec432d6ab720d83ebe4b668ebd1
Édouard Lopez <dev+github@edouard-lopez.com>
2012-10-15 00:37:54 +0200

    changing project name: renamimg files (WsWds -> Probe)

---
+++ application/config/probe.php
@@ -0,0 +1,72 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+| Configuration for the WsWds application.
+*/
+
+$config['mainDb'] = 'wswds';
+// $config['wswds:username'] = 'edouard.lopez';
+$config['ws:username'] = '';
+// $config['wswds:password'] = 'edouard.lopez';
+$config['ws:password'] = '';
+/**
+	include(APPPATH.'libraries/WS_rev_crypt.php');
+	$crypt = new WS_rev_crypt('database_root');
+//	$crypt->write('P@$$w0rd');
+	$config['ws:password'] = $crypt->read();
+	unset($crypt);
+**/
+
+// Locale/language to use for the interface
+$config['ws:locale'] = 'fr';
+
+/*
+*redirection URLs for the admin area
+*/
+// landing page after user is successfully authentified
+$config['admin_dashboard']	= "/admin/admin/";
+// Login page, un-authentified users will land on this page to provide their credentials
+$config['admin_connexion']	= "/admin/admin/connexion";
+
+
+$config['require_directories']	= array("entity", "exceptions");
+$config['require_blacklist'] = array( "Address");
+
+
+
+
+
+
+
+//###################################### REQUIRES ######################################
+$absoluteAppPath = str_replace(
+        end(explode('/', $_SERVER['SCRIPT_FILENAME'])),
+        '',
+        $_SERVER['SCRIPT_FILENAME']
+    )
+    .''.APPPATH;
+// echo $absoluteAppPath."<br/>";
+
+
+foreach($config['require_directories'] as $unDossier) {
+	require_once_file_autoload($absoluteAppPath."".$unDossier , $config['require_blacklist']);
+}
+
+
+function require_once_file_autoload($chemin , $exclude = array() ) {
+
+	$contenuDossier = scandir($chemin);
+	foreach ($contenuDossier as $unElt) {
+		$unEltComplet = "$chemin/$unElt";
+
+		if (  $unElt != "."
+		&& $unElt != ".."
+		&& !in_array($unElt.".php", $exclude) ) {
+			if(is_dir($unEltComplet)) {
+				a($unEltComplet);
+			}
+			else {
+				require_once $unEltComplet;
+			}
+		}
+	}
+}
