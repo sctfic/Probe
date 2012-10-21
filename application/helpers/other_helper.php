0cbf92b5a321e3d69f584523a7710589b5ed8fec
Édouard Lopez <dev+github@edouard-lopez.com>
2012-10-22 00:03:39 +0200

    default password length: 6 -> 8

--- application/helpers/other_helper.php
+++ application/helpers/other_helper.php
@@ -4,7 +4,7 @@
 * @param size of string
 * @param list of autorized char
 **/
-function randomPassword($size=6, $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789") {
+function randomPassword($size=8, $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789") {
 	for ($i = 0; $i < $size; $i++) {
 		$n = rand(0, strlen($alphabet)-1);
 		$pass[$i] = $alphabet[$n];
