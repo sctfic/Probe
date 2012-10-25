5770f1c78c82b9089c2006d9062e9a607505abf6
Édouard Lopez <dev+github@edouard-lopez.com>
2012-10-26 01:30:20 +0200

    default route to the installer

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,7 +38,7 @@
 |
 */
 
-$route['default_controller'] = "admin/admin";
+$route['default_controller'] = "setup/installer";
 // $route['default_controller'] = "welcome";
 $route['404_override'] = '';
 
