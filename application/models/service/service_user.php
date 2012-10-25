a54f39267681991fbafe74d8e13cf1c8398a4289
Édouard Lopez <dev+github@edouard-lopez.com>
2012-10-26 01:37:12 +0200

    indentation

--- application/models/service/service_user.php
+++ application/models/service/service_user.php
@@ -2,48 +2,50 @@
 require_once APPPATH."models/service/service.php";
 
 class Service_User extends Service {
-    function __construct() {
-        parent::__construct();
+	function __construct() {
+		parent::__construct();
 
-        //Daos
-        $this->load->model('dao/Dao_User');
-        $this->load->library('encrypt');
-        $this->encrypt->set_cipher(MCRYPT_BLOWFISH);
-    }
+		//Daos
+		$this->load->model('dao/Dao_User');
+		$this->load->library('encrypt');
+		$this->encrypt->set_cipher(MCRYPT_BLOWFISH);
+	}
 
 	/*
 	* return an User object when for authentified user, otherwise throw an error
 	*/
-    public function authentify($userName, $userPassword) {
-        $this->load->library('encrypt');
-    	$user = $this->Dao_User->read($userName, $this->encrypt->encode($userPassword) );
+	public function authentify($userName, $userPassword) {
+		$this->load->library('encrypt');
+		$user = $this->Dao_User->read($userName, $this->encrypt->encode($userPassword) );
 
-    	if($user == NULL) {
-    		throw new BusinessException( i18n('login.fail.username.password.incorrect') );
-    	}
+		if($user == NULL) {
+			throw new BusinessException( i18n('login.fail.username.password.incorrect') );
+		}
 
-    	$user->setAuthentified(true);
-    	return $user;
-    }
+		$user->setAuthentified(true);
+		return $user;
+	}
 
-    /*
-    * return an User object when for authentified user, otherwise throw an error
-    */
-    public function register($userName, $userPassword, $firstName = null, $familyName = null, $email = null, $role = 0) {
-        $this->load->library('encrypt');
-        $user = $this->Dao_User->write(
-            $userName, $this->encrypt->encode($userPassword),
-            $firstName, $familyName, 
-            $email, 
-            $role
-        );
+	/*
+	* return an User object when for authentified user, otherwise throw an error
+	*/
+	public function register($userName, $userPassword, $firstName = null, $familyName = null, $email = null, $role = 0) {
+		$this->load->library('encrypt');
+		$user = $this->Dao_User->write(
+			$userName, 
+			$this->encrypt->encode($userPassword),
+			$firstName, 
+			$familyName, 
+			$email, 
+			$role
+		);
 
-        if($user == NULL) {
-            throw new BusinessException( i18n('register.fail.username.password.incorrect') );
-        }
+		if($user == NULL) {
+			throw new BusinessException( i18n('register.fail.username.password.incorrect') );
+		}
 
-        $user->setRegistered(true);
-        return $user;
-    }
+		$user->setRegistered(true);
+		return $user;
+	}
 }
 ?>
