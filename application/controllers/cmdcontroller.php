bb2ea77d5ffbc9d0a194384bf0b3c9740827d8d8
Édouard Lopez <dev+github@edouard-lopez.com>
2012-11-04 13:00:13 +0100

    refactoring: WS -> station, weatherstation.php -> station.php

--- application/controllers/cmdcontroller.php
+++ application/controllers/cmdcontroller.php
@@ -16,9 +16,9 @@ class cmdController extends CI_Controller {
 		* Elle sera disponible sous la denominatiosn : $this->db->*
 		**/
 		include_once(BASEPATH.'core/Model.php'); // need for load models manualy
-		include_once(APPPATH.'models/weatherstation.php');
+		include_once(APPPATH.'models/station.php');
 
-		$this->WS = new weatherstation();
+		$this->station = new Station();
 	}
 
 
@@ -37,12 +37,12 @@ class cmdController extends CI_Controller {
 	function hilowsCollectors($station = null) {
 		where_I_Am(__FILE__,__CLASS__,__FUNCTION__,__LINE__,func_get_args());
 		try {
-			$item_ID = is_numeric($station) ? array_search($station, $this->WS->lst) : $station;
+			$item_ID = is_numeric($station) ? array_search($station, $this->station->stationsList) : $station;
 			if (isset($item_ID)) {
 				// on rapelle cette meme fonction mais avec de vrai paarametre : Toutes les stations
 				// on recupere les confs de $station
-				$itemConf = end($this->WS->config($item_ID)); // $station est le ID ou le nom
-				$this->WS->HilowsCollector ($itemConf);
+				$itemConf = end($this->station->config($item_ID)); // $station est le ID ou le nom
+				$this->station->HilowsCollector ($itemConf);
 				return false;
 			}
 			else return false;
@@ -57,12 +57,12 @@ class cmdController extends CI_Controller {
 	function curentCollectors($station = null) {
 		where_I_Am(__FILE__,__CLASS__,__FUNCTION__,__LINE__,func_get_args());
 		try {
-			$item_ID = is_numeric($station) ? array_search($station, $this->WS->lst) : $station;
+			$item_ID = is_numeric($station) ? array_search($station, $this->station->stationsList) : $station;
 			if (isset($item_ID)) {
 				// on rapelle cette meme fonction mais avec de vrai paarametre : Toutes les stations
 				// on recupere les confs de $station
-				$itemConf = end($this->WS->config($item_ID)); // $station est le ID ou le nom
-				$this->WS->LpsCollector ($itemConf);
+				$itemConf = end($this->station->config($item_ID)); // $station est le ID ou le nom
+				$this->station->LpsCollector ($itemConf);
 				return false;
 			}
 			else return false;
@@ -83,18 +83,18 @@ class cmdController extends CI_Controller {
 			}
 			return false;
 		}
-		elseif ($station===null && !empty($this->WS->lst)) {
+		elseif ($station===null && !empty($this->station->stationsList)) {
 			// on rapelle cette meme fonction mais avec de vrai paarametre : Toutes les stations
-			$this->dataCollectors (array_keys ($this->WS->lst));
+			$this->dataCollectors (array_keys ($this->station->stationsList));
 			return false;
 		}
 //		else return false;
 		try {
 			// on recupere les confs de $station
-			$conf = end($this->WS->config($station)); // $station est le ID ou le nom
+			$conf = end($this->station->config($station)); // $station est le ID ou le nom
 			
 			// on lance la recup des Archives de cette station
-			$this->WS->ArchCollector($conf);
+			$this->station->ArchCollector($conf);
 		}
 		catch (Exception $e) {
 			log_message('warning',  $e->getMessage());
@@ -112,23 +112,23 @@ class cmdController extends CI_Controller {
 			}
 			return false;
 		}
-		elseif ($station===null && !empty($this->WS->lst)) {
+		elseif ($station===null && !empty($this->station->stationsList)) {
 			// on rapelle cette meme fonction mais avec de vrai parametre : les ID de toutes les stations
-			$this->configCollectors (array_keys ($this->WS->lst));
+			$this->configCollectors (array_keys ($this->station->stationsList));
 			return false;
 		}
 //		else return false;
 		try {
-			$conf = end($this->WS->config($station));
+			$conf = end($this->station->config($station));
 			log_message('count', count($conf));
 
 			if (count($conf)<30 or $force==true) {
-				$readconf = $this->WS->ConfCollector($conf);
+				$readconf = $this->station->ConfCollector($conf);
 				foreach ($readconf as $key => $val) {
 					if (strpos($key, 'TR:Config:')!==FALSE)
 						$ToStoreConfig[str_replace('TR:Config:', '', $key)] = $val;
 				}
-				$this->WS->arrays2dbconfs($conf['_id'], $ToStoreConfig);
+				$this->station->arrays2dbconfs($conf['_id'], $ToStoreConfig);
 			}
 		}
 		catch (Exception $e) {
@@ -144,14 +144,14 @@ class cmdController extends CI_Controller {
 		try {
 			include_once(APPPATH.'models/db_builder.php');
 			include(APPPATH.'config/db-default.php');
-			$newID = current ($this->WS->availableID()); // prend le 1er ID vide parmis ceux disponible
+			$newID = current ($this->station->availableID()); // prend le 1er ID vide parmis ceux disponible
 
 			$dbb = new db_builder('mysql','nbv4023','root','localhost',3306,'');
 			$dbb->createAppDb($newID);
 			$dsn = $dbb->getDsn();
 
-			$this->WS->arrays2dbconfs($newID, array_merge(array('_ip'=>'', '_port'=>'', '_name'=>'', '_type'=>''), $dsn));
-			return $this->WS->config($newID);
+			$this->station->arrays2dbconfs($newID, array_merge(array('_ip'=>'', '_port'=>'', '_name'=>'', '_type'=>''), $dsn));
+			return $this->station->config($newID);
 		}
 		catch (Exception $e) {
 			log_message('warning',  $e->getMessage());
