09b95f001f3db9cfed445003505d890c2b5b2297
Sctfic (Alban LOPEZ) <alban.lopez+github@gmail.com>
2012-11-08 22:37:41 +0100

    add 2 controller for drawing, add windrose.js

---
+++ application/controllers/drawdata.php
@@ -0,0 +1,46 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class drawData extends CI_Controller {
+/**
+Cette classe appelle les differentes requetes
+en vu de les retourner au scripte ajax qui les dessinera
+*/
+	function __construct() {
+		parent::__construct();
+		where_I_Am(__FILE__,__CLASS__,__FUNCTION__,__LINE__,func_get_args());
+
+	}
+
+/**
+
+* @
+* @param 
+* @param 
+*/
+	function index(){
+
+	}
+
+/**
+
+* @
+* @param is the sensor name (one or more)
+*/
+	function curves(){
+		$args = func_get_args();
+	    foreach ($args as $value) {
+	     	
+	    }
+	}
+
+/**
+
+* @
+* @param since is the start date of result needed
+* @param lenght is the number of day
+*/
+	function windrose($since, $lenght){
+
+	}
+
+
+}
---
+++ application/controllers/microdraw.php
@@ -0,0 +1,39 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class microDraw extends CI_Controller {
+/**
+Cette classe appelle les differentes requetes
+en vu de les retourner au scripte ajax qui les dessinera
+*/
+	function __construct() {
+		parent::__construct();
+		where_I_Am(__FILE__,__CLASS__,__FUNCTION__,__LINE__,func_get_args());
+
+	}
+
+/**
+
+* @
+* @param 
+* @param 
+*/
+	function index(){
+
+	}
+
+/**
+mini courbes sans axes ni legendes : 64px * 128px
+* @param is the sensor name
+*/
+	function curves($sensor){
+	}
+
+/**
+mini rose des vents a 8 directions, 64px sans aucune legende
+* @param hour is number of hour avg
+*/
+	function windrose($hour){
+
+	}
+
+
+}
