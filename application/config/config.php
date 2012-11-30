ec98dc1d6cc649a97f22ac4fdf1118a92158c05d
Sctfic (Alban LOPEZ) <alban.lopez+github@gmail.com>
2012-11-30 15:22:08 +0100

    base_url use automaticaly the last hostname : localhost or probe.dev

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url'] = 'http://probe.dev/';
+
+$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/';
 
 /*
 |--------------------------------------------------------------------------
