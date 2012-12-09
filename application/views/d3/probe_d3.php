eb2110861efe61272f20663e9889e5957b714526
Édouard Lopez <dev+github@edouard-lopez.com>
2012-12-09 05:49:01 +0100

    view for our firs data binder (probe_d3)

---
+++ application/views/d3/probe_d3.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* D3 binder to visualize <dataset> data
+*
+* @category D3Binder
+* @package  Probe
+* @author   Édouard Lopez <dev+probe@edouard-lopez.com>
+* @license  http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode CC-by-nc-sa-3.0
+* @link     http://probe.com/doc
+ */
+?>
+<div id="filename" class="canvas">
+    <!-- d3 content should be -dynamically- placed here -->
+    <noscript><?=i18n('warning.javascript.disable');?></noscript>
+</div>
