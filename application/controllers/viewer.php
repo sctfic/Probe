7cf1b9e80155ee7e9225d4711fdd4049c1e84ff3
Édouard Lopez <dev+github@edouard-lopez.com>
2012-12-09 05:48:26 +0100

    visualization controller. For now re-routing all request to index() function : * http://probe.dev/viewer/probe_d3 -> viewer/index("probe_d3")

---
+++ application/controllers/viewer.php
@@ -0,0 +1,63 @@
+<?php
+/**
+* Data Viewer
+*
+* @category Viewer
+* @package  Probe
+* @author   Édouard Lopez <dev+probe@edouard-lopez.com>
+* @license  http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode CC-by-nc-sa-3.0
+* @link     http://probe.com/doc
+ */
+// namespace Probe\Install;
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// require_once APPPATH."/controllers/checkSetup.php";
+require_once APPPATH."/controllers/pages.php";
+
+/**
+* Manage the installation and configuration of the application
+*
+* @category Install
+* @package  Probe
+* @author   Édouard Lopez <dev+probe@edouard-lopez.com>
+* @license  http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode CC-by-nc-sa-3.0
+* @link     http://probe.com/doc
+ */
+class viewer extends CI_Controller
+{
+    /**
+     * entry point
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        where_I_Am(__FILE__, __CLASS__, __FUNCTION__, __LINE__, func_get_args());
+    }
+
+    /**
+     * [index description]
+     *
+     * @param string $d3Binder D3js script to bind data to current page
+     *
+     * @return [type] [description]
+     */
+    public function index($d3Binder = null)
+    {
+        $this->load->helper('pages');
+
+        if (empty($d3Binder) || !isset($d3Binder)) {
+            show_404();
+        }
+
+        // build view data and fetch information to build the HTML header
+        $data = pageFetchConfig($d3Binder);
+        $data['viewer'] = true;
+        // remove the controller name
+        $data['d3Binder'] = $d3Binder;
+
+        // display the view
+        $pages = new Pages();
+        $pages->view('d3/'.$d3Binder, $data);
+    }
+}
