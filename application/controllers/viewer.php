90ccca42f033d604e42223c53469ea2fac7426da
Édouard Lopez <dev+github@edouard-lopez.com>
2012-12-17 09:35:40 +0100

    viewer controller (list of available template)+viewer list template

--- application/controllers/viewer.php
+++ application/controllers/viewer.php
@@ -50,17 +50,81 @@ class viewer extends CI_Controller
         $this->load->helper('pages');
 
         if (empty($dataBinder) || !isset($dataBinder)) {
-            show_404();
+            $this->listView();
+        } else {
+            $this->binderView($dataBinder);
         }
+    }
 
-        // build view data and fetch information to build the HTML header
+    /**
+     * prepare the view to display data and visualizer
+     *
+     * @param string $dataBinder JS script name used to bind data to the view
+     *
+     * @return view data visualization with dataBinder
+     */
+    public function binderView($dataBinder)
+    {
+        where_I_Am(__FILE__, __CLASS__, __FUNCTION__, __LINE__, func_get_args());
+
         $data = pageFetchConfig($dataBinder);
         $data['viewer'] = true;
         // remove the controller name
         $data['dataBinder'] = $dataBinder;
 
+        $data['breadcrumb'] = array(
+            'viewer.list',
+            array(
+                'status'  =>  'active',
+                'url'     =>  '/viewer/'.$dataBinder,
+                'i18n'    =>  'viewer.'.$dataBinder
+            )
+        );
+
         // display the view
         $pages = new Pages();
-        $pages->view('d3/'.$dataBinder, $data);
+        $pages->view(BINDER_DIR.$dataBinder, $data);
     }
+
+    /**
+     * create a clickable list of available views
+     *
+     * @return view list of available views
+     */
+    public function listView()
+    {
+        where_I_Am(__FILE__, __CLASS__, __FUNCTION__, __LINE__, func_get_args());
+
+        $this->load->helper('pages');
+
+        $data = pageFetchConfig('list-view');
+        // remove the controller name
+        $scannedDir = array_diff(scandir(BINDER_PATH), array('..', '.'));
+        $data['list'] = array_map(array($this, 'prepareViewList'), $scannedDir);
+
+        $data['breadcrumb'] = array(
+            array(
+                'status'  =>  'active',
+                'url'     =>  '/viewer',
+                'i18n'    =>  'viewer.list'
+            )
+        );
+        // display the view
+        $pages = new Pages();
+        $pages->view('templates/viewer-list', $data);
+    }
+
+    /**
+     * Display the list of data binder available in BINDER_PATH directory
+     *
+     * @param string  $file file or directory name
+     * @param integer $key  index
+     *
+     * @return [type] [description]
+     */
+    public function prepareViewList($file, $key = null)
+    {
+        return (strstr($file, '.php') ? substr($file, 0, -4) : null);
+    }
+
 }
