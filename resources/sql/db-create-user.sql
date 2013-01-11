f1dd2cf96b1414e95babbd960aebcb43fa668bda
Édouard Lopez <dev+github@edouard-lopez.com>
2013-01-11 22:46:01 +0100

    remove the /resources/sql directory and its content. SQL queries will be in /application/model/sql

--- resources/sql/db-create-user.sql
+++
@@ -1,6 +0,0 @@
-// creation de notre utilisateur principal
-CREATE USER 'probe'@'localhost' IDENTIFIED BY  '***';
-GRANT USAGE ON * . * TO  'probe'@'localhost' IDENTIFIED BY  '***' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
-CREATE DATABASE IF NOT EXISTS  `probe` ;
-GRANT ALL PRIVILEGES ON  `probe` . * TO  'probe'@'localhost';
-
