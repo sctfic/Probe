bc9497b9bb8c605e1adb6256bc987d27fde695bc
Sctfic (Alban LOPEZ) <alban.lopez+github@gmail.com>
2013-02-12 23:30:01 +0100

    add page currents step2

---
+++ application/controllers/data.php
@@ -0,0 +1,161 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Data extends CI_Controller {
+/**
+Cette classe appelle les differentes requetes
+en vu de les retourner au scripte ajax qui les dessinera
+*/
+
+	protected $Station=NULL; // name or ID nbr of station
+	protected $sensor=NULL; // name or ID nbr of station
+	protected $Since=NULL; // start date of data
+	protected $StepUnit='HOUR'; // HOUR, DAY, WEEK, MONTH, (YEAR)
+	protected $StepNbr=12; // number of step 1-48
+	public $dataReader=NULL;
+
+	function __construct() {
+		parent::__construct();
+		where_I_Am(__FILE__,__CLASS__,__FUNCTION__,__LINE__,func_get_args());
+		
+		include_once(BASEPATH.'core/Model.php'); // need for load models manualy
+		include_once(APPPATH.'models/station.php');
+		include_once(APPPATH.'models/dao/dao_data.php');
+		include_once(APPPATH.'models/dao/dao_data_summary.php');
+
+		$this->load->helper('download');
+
+		$this->station = new Station();
+		// print_r($this->input->get());
+		// encoded with javascript encodeURIComponent()
+		$station = rawurldecode($this->input->get('station'));
+
+		$this->setSensor($this->input->get('sensor'));
+		// $this->Size = $this->input->get('Size');
+		$this->Since = rawurldecode($this->input->get('Since'));
+		$this->StepUnit = $this->input->get('StepUnit');
+		$this->StepNbr = $this->input->get('StepNbr');
+
+		if (empty($this->Since) || empty($this->StepUnit) || empty($this->StepNbr)) {
+			$this->Since = date('Y/m/d H:i:s', strtotime(date('Y/m/d 00:00:00')) - 365*24*60*60); // today - 365 days
+			$this->StepUnit = 'DAY';
+			$this->StepNbr = 365; // 365
+		}
+
+		$this->Station = end($this->station->config($station));
+		// print_r($this->Station);
+		$this->info = array("info"=>array(
+			"lat"=>$this->Station['Geo:Latitude:NordValue'],
+			"lon"=>$this->Station['Geo:Longitude:EstValue'],
+			"alt"=>$this->Station['Geo:Elevation:Ocean'],
+			"name"=>$this->Station['_name'],
+			"id"=>$this->Station['_name']));
+
+		if ($this->Since or $this->StepUnit or $this->StepNbr) {
+			$this->dataReader = new dao_data($this->Station);
+			// $this->load->model('dao/dao_data', 'dataReader'); // return $this->dataReader object
+		}
+		else {
+			$this->dataReader = new dao_data_summary($this->Station);
+			// $this->load->model('dao/dao_data_summary', 'dataReader'); // return $this->dataReader object
+		}
+
+	}
+	function setSensor($str) {
+		$this->sensor =  rawurldecode($str);
+		// if (isset($this->sensors[16])) unset($this->sensors[16]);
+		return $this->sensor;
+	}
+/**
+
+* @
+* @param 
+* @param 
+*/
+	function index(){
+		where_I_Am(__FILE__,__CLASS__,__FUNCTION__,__LINE__,func_get_args());
+
+		// header("Content-Type: application/json");
+		// echo ($this->windRose());
+		// $data = $this->windRose();
+		// ob_clean();
+		// header_remove();
+		// force_download('data.json', '{"info":["lon":0,"lat":0,"name":"name","id":"id"],"data":'.$data.'}');
+	}
+
+/**
+
+* @
+* @param since is the start date of result needed
+* @param lenght is the number of day
+* @param is the sensor name (one or more)
+*/
+	function curve(){
+		where_I_Am(__FILE__,__CLASS__,__FUNCTION__,__LINE__);
+		$data = $this->dataReader->curve (
+			tableOfSensor($this->sensor),
+			$this->sensor,
+			$this->Since,
+			$this->StepUnit,
+			$this->StepNbr
+		);
+		//print_r(array_merge(array('date', 'price'),$data));
+        $this->dl_tsv ($data);
+
+	}
+/**
+
+* @
+* @param since is the start date of result needed
+* @param lenght is the number of day
+* @param is the sensor name (one or more)
+*		 for each we return by period :
+*			[first period value,
+*			min period value,
+*			avg period value,
+*			max period value,
+*			last period value]
+*/
+	function bracketCurve(){
+		where_I_Am(__FILE__,__CLASS__,__FUNCTION__,__LINE__);
+		$data = $this->dataReader->curve (
+			tableOfSensor($this->sensor),
+			$this->sensor,
+			$this->Since,
+			$this->StepUnit,
+			$this->StepNbr
+		);
+		$this->dl_tsv ($data);
+	}
+/**
+http://probe.dev/draw/windrose?station=VP2_GTD&sensors=TA:Arch:Temp:Out:Average&Since=2012-10-26T00:00:00&StepUnit=DAY&StepNbr=6
+* @
+* @param since is the start date of result needed
+* @param lenght is the number of day
+*/
+	function wind(){
+		where_I_Am(__FILE__,__CLASS__,__FUNCTION__,__LINE__,func_get_args());
+		$data = $this->dataReader->wind ($this->Since, $this->StepUnit, $this->StepNbr);
+		$this->dl_json ($data);
+	}
+
+	private function dl_json ($data) {
+		$json = json_encode(array_merge($this->info, array('data' => $data)), JSON_NUMERIC_CHECK);
+		// ob_clean();
+		@ob_end_clean();
+		header_remove();
+		force_download('data.json', $json);
+	}
+
+	private function dl_tsv ($data) {
+	    $j = count($data);
+	    $tsv = '';
+	    if ($j<=366*288) {
+		    for ($i=0;$i<$j;$i++) {
+				$tsv .= substr($data[$i]['utc'],0,-3)."\t".$data[$i]['value']."\n";
+			}
+    		// ob_clean();
+    		@ob_end_clean();
+    		header_remove();
+    		force_download('data.tsv', "date\tval\n".trim($tsv,"\n"));
+        }
+	}
+}
---
+++ application/views/d3/currents.php
@@ -0,0 +1,218 @@
+<?php
+/**
+* D3 binder to visualize <dataset> data
+*
+* @category D3Binder
+* @package  Probe
+* @author   Édouard Lopez <dev+probe@edouard-lopez.com>
+* @license  http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode CC-by-nc-sa-3.0
+* @link     http://probe.com/doc
+ */
+// data getter
+// http://probe.dev/draw?station=VP2_GTD&sensors=TA:Arch:Temp:Out:Average&Since=2012-10-26T00:00:00&StepUnit=WEEK&StepNbr=6
+?>
+<style>
+
+</style>
+<div id="filename" class="canvas" style="clear:both;">
+
+<table id="" cellpadding="0" cellspacing="0" border="0" summary="">
+  <tbody>
+  <tr><td colspan="6" class="summary_timestamp"></td></tr>
+ <tr>
+    <td id="" class="summary_header_label">Station Summary</td>
+    <td id="" class="summary_header">Current</td>
+    <td id="" colspan="2" class="summary_header">Today's Highs</td>
+    <td id="" colspan="2" class="summary_header">Today's Lows</td>
+  </tr>
+  <tr><td colspan="6"><hr class="ThinLine"></td></tr>
+  <tr>
+    <td id="" class="val">Outside Temp</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+  <tr>
+    <td id="" class="val">Outside Humidity</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+  <tr><td colspan="6"><hr class="ThinLine"></td></tr>
+  <tr>
+    <td id="" class="val">Inside Temp</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+  <tr>
+    <td id="" class="val">Inside Humidity</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+  <tr><td colspan="6"><hr class="ThinLine"></td></tr>
+  <tr>
+    <td id="" class="val">Heat Index</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+  <tr>
+    <td id="" class="val">Wind Chill</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+  <tr>
+    <td id="" class="val">Dew Point</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+  <tr><td colspan="6"><hr class="ThinLine"></td></tr>
+  <tr>
+    <td id="" class="val">Barometer</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+  <tr>
+    <td id="" class="val">Bar Trend</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+  <tr><td colspan="6"><hr class="ThinLine"></td></tr>
+  <tr>
+    <td id="" class="val">Wind Speed</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+  <tr>
+    <td id="" class="val">Wind Direction</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+  <tr><td colspan="6"><hr class="ThinLine"></td></tr>
+    <tr>
+    <td id="" class="val">Solar Radiation</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+  <tr>
+    <td id="" class="val">UV Radiation</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+  <tr><td colspan="6"><hr class="ThinLine"></td></tr>
+    <tr>
+    <td id="" class="val">12 Hour Forecast</td>
+    <td id="" class="val" colspan="5">Mostly Clear</td>
+  </tr>
+  <tr><td colspan="6"><hr class="ThinLine"></td></tr>
+  <tr>
+    <td id="" class="summary_header_label">Wind</td>
+    <td id="" class="summary_header">2 Minute</td>
+    <td id="" class="summary_header">10 Minute</td>
+    <td id="" class="summary_header">&nbsp;</td>
+    <td id="" class="summary_header">&nbsp;</td>
+    <td id="" class="summary_header">&nbsp;</td>
+  </tr>
+  <tr><td colspan="6"><hr class="ThinLine"></td></tr>
+  <tr>
+    <td id="" class="val">Average Wind Speed</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+  <tr>
+    <td id="" class="val">Wind Gust Speed</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+  <tr><td colspan="6"><hr class="ThinLine"></td></tr>
+  <tr>
+    <td id="" class="summary_header_label">Rain</td>
+    <td id="" class="summary_header">Rate</td>
+    <td id="" class="summary_header">Day</td>
+    <td id="" class="summary_header">Storm</td>
+    <td id="" class="summary_header">Month</td>
+    <td id="" class="summary_header">Year</td>
+  </tr>
+  <tr><td colspan="6"><hr class="ThinLine"></td></tr>
+  <tr>
+    <td id="" class="val">Rain</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+  <tr>
+    <td id="" class="val">Last Hour Rain</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+    <tr>
+    <td id="" class="val">ET</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+    <td id="" class="val">-</td>
+  </tr>
+    <tr><td colspan="6"><hr class="ThinLine"></td></tr>
+  </tbody></table>
+
+    <noscript><?=i18n('warning.javascript.disable');?></noscript>
+</div>
+<script src="http://d3js.org/d3.v3.js"></script>
+<script>
+$(document).ready(function(){
+    var url = "http://probe.dev/data/curents?station=VP2_GTD";
+    // $.getJSON(url, function(json) {
+    d3.tsv(url, function(error, tsv) {
+        if (error) return console.warn(error);
+        else return false;
+   });
+});
+</script>
