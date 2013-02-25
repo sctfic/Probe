f13914dbdc9a71c4015545efbfa5108f2ff68f59
Sctfic (Alban LOPEZ) <alban.lopez+github@gmail.com>
2013-02-25 14:21:02 +0100

    Currents display with treeView (jsTree)

--- application/views/d3/currents.php
+++ application/views/d3/currents.php
@@ -12,60 +12,15 @@
 // http://probe.dev/draw?station=VP2_GTD&sensors=TA:Arch:Temp:Out:Average&Since=2012-10-26T00:00:00&StepUnit=WEEK&StepNbr=6
 ?>
 <style>
-	.da-center {margin: 0 auto;}?#JQTreeview
-	{
-	  border: 1px solid #11B700;
-	  padding: 5px;
-	}
-
-	.expandNode
-	{
-	  cursor: pointer;
-	  width: 16px;
-	  height: 16px;
-	  background-color: #000;
-	  float: left;
-	  margin-right: 8px;
-	}
-
-	.expand
-	{
-	  background: url(http://www.switchonthecode.com/sites/default/files/707/images/expandIcon.png) no-repeat;
-	}
-
-	.collapse
-	{
-	  background: url(http://www.switchonthecode.com/sites/default/files/707/images/collapseIcon.png) no-repeat;
-	}
-
-	.NodeContents
-	{
-	  border-left: 6px solid #D2FFCC;
-	  margin-left: 4px;
-	  display: none;
-	  padding-left: 5px;
-	}
-
-	span.ItemTitle
-	{
-	  font-size: 10px;
-	  width: 100px;
-	  text-decoration: underline;
-	}
-
-	div.NodeItem div.ItemTxt
-	{
-	  font-size: 14px;
-	  padding: 2px 5px;
-	  margin-bottom: 2px;
-	  text-decoration: none;
-	}
 
 </style>
 <div id="filename" class="canvas" style="clear:both;">
-	<div id="Treeview">	</div>
+	<div id="Tree">	</div>
 	<noscript><?=i18n('warning.javascript.disable');?></noscript>
 </div>
+
+<script type="text/javascript" src="http://static.jstree.com/v.1.0pre/jquery.jstree.js"></script>
+
 <script>
 // http://jquery.bassistance.de/treeview/demo/
 
@@ -76,11 +31,16 @@ $(document).ready(function(){
 	// $.getJSON(url, function(json) {
 	$.getJSON(url, function(data) {
 		 
-		$.each(data, function(key, val) { str = ''; recursiveFunction(key, val) });
+		$.each(data, function(key, val) { str = ''; recursiveFunction(key, val, 'Tree') });
 		console.log(str);
-		$('#Treeview').append(str);
-		$('#Treeview > ul').fadeToggle({ duration: 200 });
-		//$('ul li:hidden:first').delay(50).fadeIn(fadeItem);
+		$('#Tree').append('<ul>'+str+'</ul>');
+		// $(function () {
+	    	$("#Tree").jstree({
+				"plugins" : [ "themes", "html_data" ]
+			});
+		// });
+		console.log('end');
+
     });
 });
 </script>
--- resources/js/binders/currents.js
+++ resources/js/binders/currents.js
@@ -1,11 +1,14 @@
 	var str = '';
-    function recursiveFunction(key, val) {
-       	var type = $.type (val);
-        if (type==='object' || type==='array') {
-        	str += '<li id="' + key + '" class="' + type + '">' + key + '<ul>';
-            $.each(val, function(k, v) { recursiveFunction(k, v) });
-            str += '</ul></li>'
-        }
-        else str += '<li id="' + key + '" class="' + type + '">' + key + ' = ' + val + '</li>';
-    }
+	function recursiveFunction(key, val, id) {
+		var type = $.type (val);
+		id += '_'+key;
+		if (type==='object' || type==='array') {
+			str += '<li id="' + id + '" class="' + type + '"><a href="#">' + key + "</a>\n";
+			str += "<ul>\n";
+				$.each(val, function(k, v) { recursiveFunction(k, v, id); });
+			str += "</ul>\n";
+			str += "</li>\n";
+		}
+		else str += '<li id="' + id + '" class="' + type + '"><a href="#">' + key + ' = ' + val + "</a></li>\n";
+	}
 
