cbfef16641b818cd0d82791fc1c86c03497bb056
Édouard Lopez <dev+git@edouard-lopez.com>
2013-05-11 01:30:33 +0200

    style(list-station): indentation, floating nav, remove duplicate file
    Signed-off-by: Édouard Lopez <dev+git@edouard-lopez.com>

--- application/views/configuration/list-stations.php
+++ application/views/configuration/list-stations.php
@@ -1,48 +1,46 @@
-<div class="navbar navbar-inverse navbar-fixed-top">
-	<ul class="breadcrumb">
-	    <li><a href="/configuration/stations-list"><?=i18n("configuration.stations.breadcrumb")?></a> <span class="divider">/</span></li>
-	</ul>
-</div>
-
-
 <div class="container">
-	<!-- <div class="row"> -->
-		<article class="row-fluid">
-			<section id="stations-list" class="span3">
-				<nav>
-					<ol>
-						<?php
-						if (!empty($stationsConf)){
-						 	foreach ($stationsConf as $_name=>$conf){
-						 		?>
-								<li><?=$_name?></li>
-								<?php
-							}
-						}
-						else {
-							?>
-							<li><a href="/configuration/add-station"><?=i18n('configuration.stations.add-new.station')?></a></li>
-							<?php
-						}
-						?>
-					</ol>
-				</nav>
-			</section>
-			<section id="stations-form" class="span4">
-				<?php
-				if (!empty($stationsConf)){
-					foreach ($stationsConf as $_name => $conf) {
-						$confs['confs']=$conf;
-						$this->load->view('configuration/stations-form', $confs);
-					}
-				}
-				?>
-				<!-- <a href="#" class="btn"><i class="fam-add"></i> Add New Station</a> -->
-				<a href="/configuration/add-station" class="btn btn-success">
-					<i class="icon-white icon-plus"></i>
-					<?=i18n('configuration.station.add-new')?>
-				</a>
-			</section>
-		</article>
-	<!-- </div> -->
+    <!-- <div class="row"> -->
+    <article class="row-fluid">
+        <section id="stations-list" class="span3"
+                 data-spy="affix" data-offset-top="200">
+            <nav>
+                <ol>
+                    <?php
+                    if (!empty($stationsConf)) {
+                        foreach ($stationsConf as $_name => $conf) {
+                            ?>
+                            <li><?=$_name?></li>
+                            <?php
+                        }
+                    } else {
+                        ?>
+                        <li>
+                            <a href="/configuration/add-station">
+                                <?=i18n('configuration.stations.add-new.station')?>
+                            </a>
+                        </li>
+                        <?php
+                    }
+                    ?>
+                </ol>
+            </nav>
+        </section>
+        <section id="stations-form" class="span4">
+            <?php
+            if (!empty($stationsConf)) {
+                foreach ($stationsConf as $_name => $conf) {
+                    $confs['confs'] = $conf;
+                    $this->load->view('configuration/stations-form', $confs);
+                }
+            }
+            ?>
+            <!-- <a href="#" class="btn"><i class="fam-add"></i> Add New Station</a> -->
+            <a href="/configuration/add-station" class="btn btn-success">
+                <i class="icon-white icon-plus"></i>
+                <?=i18n('configuration.station.add-new')?>
+            </a>
+        </section>
+    </article>
+    <!-- </div> -->
 </div>
+<script src="/resources/js/libs/bootstrap/bootstrap-affix.js"/>
