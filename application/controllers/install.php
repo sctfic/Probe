bf034833f49f8ee48d7c5321e4c90dd678935458
Édouard Lopez <dev+git@edouard-lopez.com>
2013-05-14 15:42:55 +0200

    refactor(i18n,install): use context keyword: install
    unify key's keyword: setup -> install

--- application/controllers/install.php
+++ application/controllers/install.php
@@ -131,7 +131,7 @@ class Install extends CI_Controller {
         $page = new Page_manager();
 
         // build view data
-        $data = $page->fetchConfig('setup-dbms'); // fetch information to build the HTML header
+        $data = $page->fetchConfig('install.dbms'); // fetch information to build the HTML header
         $data['breadcrumb'] = $this->_breadcrumb['dbms'];
         $data['dbmsUsername'] = null;
         $data['dbmsPassword'] = null;
@@ -226,7 +226,7 @@ class Install extends CI_Controller {
         $page = new Page_manager();
 
         // build view data
-        $data = $page->fetchConfig('setup-admin-user'); // fetch information to build the HTML header
+        $data = $page->fetchConfig('install.admin-user'); // fetch information to build the HTML header
         $data['breadcrumb'] = $this->_breadcrumb['admin-user'];
 
         $data['adminUsername'] = null;
