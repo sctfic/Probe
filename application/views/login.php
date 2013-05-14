288d2b90ccdf25b25d4719f498f8a382d2061fac
Édouard Lopez <dev+probe@edouard.lopez.com>
2013-05-14 16:04:49 +0200

    feat(i18n,ui): add i18n key for weather condition
    This file should be removed in the future, when we got a list of icons or
    something equivalent that embed and provide a list from which we can
    extract the possible weather conditions.

--- application/views/login.php
+++ application/views/login.php
@@ -69,7 +69,7 @@
 
     <aside class="js time-indicator">
       <div class="now"><?=date('Y-m-d H:i')?>
-        <!-- <img class="time-indicator" src="/themes/icons/weather-clear.png" alt="<?=i18n("sunny weather")?>?>" /> -->
+        <!-- <img class="time-indicator" src="/themes/icons/weather-clear.png" alt="<?=i18n("weather.sunny")?>?>" /> -->
       </div>
     </aside>
 
