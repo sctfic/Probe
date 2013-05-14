b6a5bb27bebef3ca3d14ffd6d574a12730ecffe6
Édouard Lopez <dev+git@edouard-lopez.com>
2013-05-14 09:40:45 +0200

    refactor(dev,jquery): condition use of web API to production-only
    Add constant 'DEV_HOST', the development hostname as returned by $_SERVER["HTTP_HOST"].
    Default value is set to 'probe.dev'.
    This constant imply that call to Google CDN version of jQuery happen only on production. Thus
    directly calling the local lib, in order to speed up page load when working offline or on slow
    connection.

--- application/views/templates/js-libs.php
+++ application/views/templates/js-libs.php
@@ -10,12 +10,14 @@
  */
 ?>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<?php if ($_SERVER["HTTP_HOST"]	!= DEV_HOST): ?>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<?php endif ?>
 <script>window.jQuery || document.write('<script src="/resources/js/libs/jquery-1.8.3.min.js"><\/script>')</script>
 <script src="/resources/js/libs/jquery.bgpos.js"></script>
 
-<!--<script src="/resources/js/libs/bootstrap.min.js"></script>-->
-<script src="/resources/js/libs/bootstrap.js"></script>
+<script src="/resources/js/libs/bootstrap.min.js"></script>
+<!--<script src="/resources/js/libs/bootstrap.js"></script>-->
 
 <?php if ($viewer === true) : ?>
     <script src="/resources/js/libs/d3.v3.js"></script>
@@ -33,16 +35,17 @@
     <script src="/resources/js/pages/<?=$page;?>.js"></script>
 <?php endif; ?>
 
-<script type="text/javascript">
+<?php if ($_SERVER["HTTP_HOST"]	!= DEV_HOST): ?>
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-39489032-1']);
+      _gaq.push(['_setDomainName', 'no-ip.org']);
+      _gaq.push(['_trackPageview']);
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-39489032-1']);
-  _gaq.push(['_setDomainName', 'no-ip.org']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
+<?php endif; ?>
